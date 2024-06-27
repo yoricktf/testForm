08688b7c8fdcd11862b3dd3eba6f4a751c4adf6b
yoricktf <yorick.tenfeld@gmail.com>
2024-06-27 14:05:14 +0200

    created summary page and displaying all data entered by the user and the ability to confirm which takes you back to the home page.

---
+++ src/pages/summary.tsx
@@ -0,0 +1,74 @@
+import { useRouter } from 'next/router';
+
+type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: number;
+  salary: string;
+};
+
+export default function Summary({ userData }: { userData: User }) {
+  const router = useRouter();
+
+  function userSubmission(e: any) {
+    e.preventDefault();
+    if (confirm('Thank you for your submission')) {
+      router.push('/');
+    }
+  }
+
+  return (
+    <>
+      <h1>Summary</h1>
+      <p>
+        Thank you {userData.firstName} for taking the time to give us your
+        information. Please take a moment to double check it now to make sure
+        there are no mistakes.
+      </p>
+      <form onSubmit={userSubmission}>
+        <label htmlFor='firstName'>First Name:</label>
+        <input
+          type='text'
+          id='firstName'
+          name='firstName'
+          value={userData.firstName}
+          disabled
+        />
+        <label htmlFor='lastName'>Last Name:</label>
+        <input
+          type='text'
+          id='lastName'
+          name='lastName'
+          value={userData.lastName}
+          disabled
+        />
+        <label htmlFor='email'>Email:</label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          value={userData.email}
+          disabled
+        />
+        <label htmlFor='phone'>Phone Number:</label>
+        <input
+          type='tel'
+          id='phone'
+          name='phone'
+          value={userData.phone}
+          disabled
+        />
+        <label htmlFor='salary'>Salary:</label>
+        <input
+          type='text'
+          id='salary'
+          name='salary'
+          value={userData.salary}
+          disabled
+        />
+        <button>Looks Good!</button>
+      </form>
+    </>
+  );
+}
